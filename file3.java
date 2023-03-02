2730667f01c47d01900788ced93f14a7fecbe7d7
divya <divyachaitanya2009@gmail.com>
2023-03-02 09:00:01 +0530

    welcome to devops statement added

--- file3.java
+++ file3.java
@@ -2,5 +2,7 @@ public class file3{
 	 public static void main(strig args[]){
 		 //print statment for hello world
 	system.out.println.("hello world");
+	  	 //print statement for welcome to devops
+	system.out.println.("welcome to devops");
 	 }
 }
